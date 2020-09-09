97471a442bb8a89153e6cec65f496676aa8a40b5
MatthewRajala <70542961+MatthewRajala@users.noreply.github.com>
2020-09-09 10:28:01 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -4,7 +4,7 @@ def majuscule(mot):
     resultat = ''
     for lettre in mot:
         # TODO completer la fonction ici
-        print(resultat)
+        
         resultat += lettre
     return mot
 
